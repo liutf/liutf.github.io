1521108bfb8e6246635f9cc74092b8601e5c928a
liutf <lautayfir@163.com>
2017-11-27 22:23:34 +0800

    ci

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Decrypt the private key
 openssl aes-256-cbc -K $encrypted_8ae64f4187a1_key -iv $encrypted_8ae64f4187a1_iv -in .travis/private_key.enc -out ~/.ssh/id_rsa -d
+
 # Set the permission of the key
 chmod 600 ~/.ssh/id_rsa
 # Start SSH agent
@@ -12,5 +13,6 @@ cp .travis/ssh_config ~/.ssh/config
 # Set Git config
 git config --global user.name "liutf"
 git config --global user.email lautayfir@163.com
+
 # Deploy to GitHub
 hexo deploy
